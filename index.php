418557c534fc8378e2f94445cbd6c89abeaae78b
Akun-Belajar <akunbelajaar1@gmail.com>
2024-11-23 03:03:47 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
   <h1>hello world</h1>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas assumenda autem, harum dignissimos optio iste ut dolore repellendus quod, magni dolorum quae tempore aspernatur, alias aut a. Ullam, laudantium ea?</p>
 </body>
 </html>
